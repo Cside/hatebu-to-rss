276ac0a9813be7c8d16a7e13f529d826233a51f8
Cside <cside.story@gmail.com>
2022-10-09 23:26:32 +0900

    commit

--- dist/app.js
+++ dist/app.js
@@ -1,36 +1,31 @@
-import express = require('express');
-import { Feed } from 'feed';
-import { JSDOM } from 'jsdom';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const feed_1 = require("feed");
+const jsdom_1 = require("jsdom");
 const morgan = require('morgan');
-
 const app = express();
 app.use(morgan('combined'));
-
-
-app.get('/:userID', async (req: express.Request, res: express.Response) => {
+app.get('/:userID', async (req, res) => {
     const userID = req.params.userID;
-
     const httpRes = await fetch(`https://b.hatena.ne.jp/${userID}`);
     if (httpRes.status === 404) {
         res.status(400);
         return res.send(`User "${userID}" is not found`);
-    } else if (httpRes.status !== 200) {
+    }
+    else if (httpRes.status !== 200) {
         res.status(503);
         return res.send(`Some error occured. code: ${httpRes.status}`);
     }
     const html = await httpRes.text();
-
-    const dom = new JSDOM(html);
-    const document = dom.window.document
-
-    const feed = new Feed({
+    const dom = new jsdom_1.JSDOM(html);
+    const document = dom.window.document;
+    const feed = new feed_1.Feed({
         title: `${userID}のブックマーク`,
         id: `https://b.hatena.ne.jp/${userID}`,
         link: `https://b.hatena.ne.jp/${userID}`,
         copyright: 'All rights reserved 2022, Mochi',
     });
-
     for (const article of document.querySelectorAll('.bookmark-item')) {
         const title = article.querySelector('.centerarticle-entry-title a')?.textContent ?? '';
         const url = article.querySelector('.centerarticle-entry-title a')?.getAttribute('href') ?? '';
@@ -47,10 +42,9 @@ app.get('/:userID', async (req: express.Request, res: express.Response) => {
             description: comment,
             content: comment,
             date,
-        })
+        });
     }
     res.type('application/xml');
     res.send(feed.atom1());
 });
-
-app.listen(process.env.PORT);
+app.listen(3000);
