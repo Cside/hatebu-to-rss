84605d6b083e16c313876824f79c34772a87ea6c
Cside <cside.story@gmail.com>
2022-10-09 13:12:41 +0900

    完成してしまった...

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,56 @@
 import express = require('express');
+import { Feed } from 'feed';
+import { JSDOM } from 'jsdom';
+
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('combined'));
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    req.query.id = req.query.user
-    res.status(204).send();
+
+app.get('/:userID', async (req: express.Request, res: express.Response) => {
+    const userID = req.params.userID;
+
+    const httpRes = await fetch(`https://b.hatena.ne.jp/${userID}`);
+    if (httpRes.status === 404) {
+        res.status(400);
+        return res.send(`User "${userID}" is not found`);
+    } else if (httpRes.status !== 200) {
+        res.status(503);
+        return res.send(`Some error occured. code: ${httpRes.status}`);
+    }
+    const html = await httpRes.text();
+
+    const dom = new JSDOM(html);
+    const document = dom.window.document
+
+    const feed = new Feed({
+        title: `${userID}のブックマーク`,
+        id: `https://b.hatena.ne.jp/${userID}`,
+        link: `https://b.hatena.ne.jp/${userID}`,
+        copyright: 'All rights reserved 2022, Mochi',
+    });
+
+    for (const article of document.querySelectorAll('.bookmark-item')) {
+        const title = article.querySelector('.centerarticle-entry-title a')?.textContent ?? '';
+        const url = article.querySelector('.centerarticle-entry-title a')?.getAttribute('href') ?? '';
+        const comment = article.querySelector('.js-comment')?.textContent ?? '';
+        const date = (() => {
+            const yyyymmdd = article.querySelector('.centerarticle-reaction-timestamp')?.textContent ?? '';
+            const [yy, mm, dd] = yyyymmdd.split('/').map(str => Number(str));
+            return new Date(yy, mm - 1, dd);
+        })();
+        feed.addItem({
+            title,
+            id: url,
+            link: url,
+            description: comment,
+            content: comment,
+            date,
+        })
+    }
+    res.type('application/xml');
+    res.send(feed.atom1());
 });
 
 app.listen(3000);
