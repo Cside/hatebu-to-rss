a2621e7b5aa8f414ebd2e1f7807ce6cc5fcabe7d
Cside <cside.story@gmail.com>
2022-10-16 21:00:34 +0900

    add link

--- dist/app.js
+++ dist/app.js
@@ -7,15 +7,15 @@ const express = require("express");
 const axios_1 = __importDefault(require("axios"));
 const feed_1 = require("feed");
 const jsdom_1 = require("jsdom");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const app = express();
-app.use(morgan('combined'));
-app.get('/:userID', async (req, res) => {
+app.use(morgan("combined"));
+app.get("/:userID", async (req, res) => {
     var _a, _b, _c, _d, _e, _f, _g, _h;
     const userID = req.params.userID;
-    if (!userID || userID === 'favicon.ico') {
+    if (!userID || userID === "favicon.ico") {
         res.status(200);
-        return res.send('OK');
+        return res.send("OK");
     }
     const httpRes = await axios_1.default.get(`https://b.hatena.ne.jp/${userID}`);
     if (httpRes.status === 404) {
@@ -33,17 +33,19 @@ app.get('/:userID', async (req, res) => {
         title: `${userID}のブックマーク`,
         id: `https://b.hatena.ne.jp/${userID}`,
         link: `https://b.hatena.ne.jp/${userID}`,
-        copyright: 'All rights reserved 2022, Mochi',
+        copyright: "All rights reserved 2022, Mochi",
     });
-    for (const article of document.querySelectorAll('.bookmark-item')) {
-        const title = (_b = (_a = article.querySelector('.centerarticle-entry-title a')) === null || _a === void 0 ? void 0 : _a.textContent) !== null && _b !== void 0 ? _b : '';
-        const url = (_d = (_c = article.querySelector('.centerarticle-entry-title a')) === null || _c === void 0 ? void 0 : _c.getAttribute('href')) !== null && _d !== void 0 ? _d : '';
-        const commentUrl = (_f = (_e = article.querySelector('.centerarticle-users a')) === null || _e === void 0 ? void 0 : _e.getAttribute('href')) !== null && _f !== void 0 ? _f : '';
-        const comment = (_h = (_g = article.querySelector('.js-comment')) === null || _g === void 0 ? void 0 : _g.textContent) !== null && _h !== void 0 ? _h : '';
+    for (const article of document.querySelectorAll(".bookmark-item")) {
+        const title = (_b = (_a = article.querySelector(".centerarticle-entry-title a")) === null || _a === void 0 ? void 0 : _a.textContent) !== null && _b !== void 0 ? _b : "";
+        const url = (_d = (_c = article
+            .querySelector(".centerarticle-entry-title a")) === null || _c === void 0 ? void 0 : _c.getAttribute("href")) !== null && _d !== void 0 ? _d : "";
+        const commentUrl = "https://b.hatena.ne.jp" +
+            ((_f = (_e = article.querySelector(".centerarticle-users a")) === null || _e === void 0 ? void 0 : _e.getAttribute("href")) !== null && _f !== void 0 ? _f : "");
+        const comment = (_h = (_g = article.querySelector(".js-comment")) === null || _g === void 0 ? void 0 : _g.textContent) !== null && _h !== void 0 ? _h : "";
         const date = (() => {
             var _a, _b;
-            const yyyymmdd = (_b = (_a = article.querySelector('.centerarticle-reaction-timestamp')) === null || _a === void 0 ? void 0 : _a.textContent) !== null && _b !== void 0 ? _b : '';
-            const [yy, mm, dd] = yyyymmdd.split('/').map(str => Number(str));
+            const yyyymmdd = (_b = (_a = article.querySelector(".centerarticle-reaction-timestamp")) === null || _a === void 0 ? void 0 : _a.textContent) !== null && _b !== void 0 ? _b : "";
+            const [yy, mm, dd] = yyyymmdd.split("/").map((str) => Number(str));
             return new Date(yy, mm - 1, dd);
         })();
         feed.addItem({
@@ -51,11 +53,11 @@ app.get('/:userID', async (req, res) => {
             id: url,
             link: url,
             description: comment,
-            content: `${comment}\nhttps://b.hatena.ne.jp${commentUrl}`,
+            content: `${comment}\n<a href="${commentUrl}" target="_blank">${commentUrl}</a>`,
             date,
         });
     }
-    res.type('application/xml');
+    res.type("application/xml");
     res.send(feed.atom1());
 });
 app.listen(process.env.PORT);
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,70 @@
-import express = require('express');
-import axios from 'axios';
-import { Feed } from 'feed';
-import { JSDOM } from 'jsdom';
+import express = require("express");
+import axios from "axios";
+import { Feed } from "feed";
+import { JSDOM } from "jsdom";
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 const app = express();
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
+app.get("/:userID", async (req: express.Request, res: express.Response) => {
+  const userID = req.params.userID;
+  if (!userID || userID === "favicon.ico") {
+    res.status(200);
+    return res.send("OK");
+  }
 
-app.get('/:userID', async (req: express.Request, res: express.Response) => {
-    const userID = req.params.userID;
-    if (!userID || userID === 'favicon.ico') {
-        res.status(200);
-        return res.send('OK');
-    }
+  const httpRes = await axios.get<string>(`https://b.hatena.ne.jp/${userID}`);
+  if (httpRes.status === 404) {
+    res.status(400);
+    return res.send(`User "${userID}" is not found`);
+  } else if (httpRes.status !== 200) {
+    res.status(503);
+    return res.send(`Some error occured. code: ${httpRes.status}`);
+  }
+  const html = await httpRes.data;
 
-    const httpRes = await axios.get<string>(`https://b.hatena.ne.jp/${userID}`);
-    if (httpRes.status === 404) {
-        res.status(400);
-        return res.send(`User "${userID}" is not found`);
-    } else if (httpRes.status !== 200) {
-        res.status(503);
-        return res.send(`Some error occured. code: ${httpRes.status}`);
-    }
-    const html = await httpRes.data;
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
 
-    const dom = new JSDOM(html);
-    const document = dom.window.document
+  const feed = new Feed({
+    title: `${userID}のブックマーク`,
+    id: `https://b.hatena.ne.jp/${userID}`,
+    link: `https://b.hatena.ne.jp/${userID}`,
+    copyright: "All rights reserved 2022, Mochi",
+  });
 
-    const feed = new Feed({
-        title: `${userID}のブックマーク`,
-        id: `https://b.hatena.ne.jp/${userID}`,
-        link: `https://b.hatena.ne.jp/${userID}`,
-        copyright: 'All rights reserved 2022, Mochi',
+  for (const article of document.querySelectorAll(".bookmark-item")) {
+    const title =
+      article.querySelector(".centerarticle-entry-title a")?.textContent ?? "";
+    const url =
+      article
+        .querySelector(".centerarticle-entry-title a")
+        ?.getAttribute("href") ?? "";
+    const commentUrl =
+      "https://b.hatena.ne.jp" +
+      (article.querySelector(".centerarticle-users a")?.getAttribute("href") ??
+        "");
+    const comment = article.querySelector(".js-comment")?.textContent ?? "";
+    const date = (() => {
+      const yyyymmdd =
+        article.querySelector(".centerarticle-reaction-timestamp")
+          ?.textContent ?? "";
+      const [yy, mm, dd] = yyyymmdd.split("/").map((str) => Number(str));
+      return new Date(yy, mm - 1, dd);
+    })();
+    feed.addItem({
+      title,
+      id: url,
+      link: url,
+      description: comment,
+      content: `${comment}\n<a href="${commentUrl}" target="_blank">${commentUrl}</a>`,
+      date,
     });
-
-    for (const article of document.querySelectorAll('.bookmark-item')) {
-        const title = article.querySelector('.centerarticle-entry-title a')?.textContent ?? '';
-        const url = article.querySelector('.centerarticle-entry-title a')?.getAttribute('href') ?? '';
-        const commentUrl = article.querySelector('.centerarticle-users a')?.getAttribute('href') ?? '';
-        const comment = article.querySelector('.js-comment')?.textContent ?? '';
-        const date = (() => {
-            const yyyymmdd = article.querySelector('.centerarticle-reaction-timestamp')?.textContent ?? '';
-            const [yy, mm, dd] = yyyymmdd.split('/').map(str => Number(str));
-            return new Date(yy, mm - 1, dd);
-        })();
-        feed.addItem({
-            title,
-            id: url,
-            link: url,
-            description: comment,
-            content: `${comment}\nhttps://b.hatena.ne.jp${commentUrl}`,
-            date,
-        })
-    }
-    res.type('application/xml');
-    res.send(feed.atom1());
+  }
+  res.type("application/xml");
+  res.send(feed.atom1());
 });
 
 app.listen(process.env.PORT);
